20124bc5794d61ed771e555171e8fc36cd237b80
jake-newbie <91320326+jake-newbie@users.noreply.github.com>
2024-05-22 15:48:53 +0700

    Update pid.h

--- Drone_FreeRTOS/Core/Inc/pid.h
+++ Drone_FreeRTOS/Core/Inc/pid.h
@@ -11,7 +11,7 @@
 #include "main.h"
 
 
-float calculate_pid(float pid_p_gain,float pid_i_gain,float pid_d_gain, float gyro_input,uint32_t channel_value);
+float calculate_pid(float pid_p_gain,float pid_i_gain,float pid_d_gain, float gyro_input,uint32_t channel_value,float* last_d_error);
 
 
 #endif /* INC_PID_H_ */
